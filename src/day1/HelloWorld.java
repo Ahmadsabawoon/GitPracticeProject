d55ff09597c4cca30c778e1693f9348431c270de
Ahmad Seyar Sabawoon <seyarkhan143@gmail.com>
2021-04-24 13:52:42 -0700

    added more work after publishing in GitHub

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -6,5 +6,6 @@ public class HelloWorld {
 
         System.out.println("Hello World");
         System.out.println("Added GitHub Now");
+        System.out.println("Yay stuff are on GitHub");
     }
 }
